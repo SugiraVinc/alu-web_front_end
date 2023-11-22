ae87365db300a3da87a18813217dd753cada2695
SugiraVinc <sugiravincentg11@gmail.com>
2023-11-22 10:20:36 +0200

    Adding paragraph

---
+++ JQuery_advanced/paragraph.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function () {
+        //call function
+        function createTextElement(){
+            $('p').append('Test');
+            window.location=document.getElementById("text").value;
+        }        
+    });
